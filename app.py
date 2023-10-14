37f7665b0f1c56b6d6ab5f7eff5f947922b20cd6
Sahan Nayanajith <sahantm0@gmail.com>
2023-10-15 04:13:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask_socketio import SocketIO
-from flask import Flask, jsonify,render_template
+from flask import Flask, jsonify,render_template, request
 from flask_mqtt import Mqtt
 from flask_mail import Mail, Message
 import json
@@ -7,7 +7,9 @@ from model import get_prediction
 import datetime
 import numpy as np
 import pandas as pd 
-import os
+3
+
+
 
 app = Flask(__name__)
 
@@ -19,14 +21,14 @@ app.config['MQTT_PASSWORD'] = ''
 app.config['MQTT_REFRESH_TIME'] = 1.0  # refresh time in seconds
 mqtt = Mqtt(app)
 
-# SocketIO1
+# SocketIO
 socketio = SocketIO(app)
 
 #  Mail Configuration
 app.config['MAIL_SERVER']='smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USERNAME'] = ''
-app.config['MAIL_PASSWORD'] = ''
+app.config['MAIL_PASSWORD'] = ""
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 mail = Mail(app)
@@ -46,30 +48,31 @@ def current_datetime():
 
 @mqtt.on_connect()
 def handle_connect(client, userdata, flags, rc):
-    mqtt.subscribe('sahan1')
+    mqtt.subscribe('wildfire1')
 
 @mqtt.on_message()
 def handle_mqtt_message(client, userdata, message):
     global mqtt_data
     mqtt_data = message.payload.decode()
     mqtt_data = json.loads(mqtt_data) 
-    mean = np.array([32.19072165, 61.80412371,  0.79175258])
-    std = np.array([ 3.643907, 14.98875123, 2.08270592])
-    scaled_data = np.divide(np.subtract(mqtt_data, mean), std)
-    prediction = predict_fire(scaled_data)
-    if(prediction == 'Fire Detected'):    
-        mqtt_data.append("You're in Danger")
-        with app.app_context():
-            print(send_mail())     
-    else:
-        mqtt_data.append("You're Safe")
-    print(mqtt_data)
-    print(scaled_data)
-    print(prediction)
-    socketio.emit('mqtt_data_update', mqtt_data)
+    prediction = predict_fire(mqtt_data)
+    send_frontend(prediction, mqtt_data)
+    
+
+@app.route('/process_data', methods=['POST'])
+def process_data():
+    if request.method == 'POST':
+        data = request.get_json()
+        prediction = predict_fire(data)
+        send_frontend(prediction, data)
+        return "successs"
+
 
 def predict_fire(array):
-    new_data = pd.DataFrame({'Temperature': [array[0]], 'RH': [array[1]], 'Rain': [array[2]]})
+    mean = np.array([32.19072165, 61.80412371,  0.79175258])
+    std = np.array([ 3.643907, 14.98875123, 2.08270592])
+    scaled_data = np.divide(np.subtract(array, mean), std)
+    new_data = pd.DataFrame({'Temperature': [scaled_data[0]], 'RH': [scaled_data[1]], 'Rain': [scaled_data[2]]})
     prediction = get_prediction().predict(new_data)
     if prediction[0] == 1:
         return "Fire Detected"
@@ -77,12 +80,24 @@ def predict_fire(array):
         return "No Fire Detected"
 
 def send_mail():
+    current_datetime = datetime.datetime.now().strftime('%Y-%m-%d %I:%M %p')
     msg = Message("Fire Warning", 
                   sender='noreply@demo.com', 
                   recipients=['sahantm0@gmail.com'])
-    msg.body="You are in danger. Fire condition detected."
+    msg.body = f"You are in danger. Fire condition detected at {current_datetime}."
     mail.send(msg)
     return "Email sent"
+
+def send_frontend(prediction, data):
+    if(prediction == 'Fire Detected'):    
+        data.append("Danger")
+        with app.app_context():
+            print(send_mail())     
+    else:
+        data.append("Safe")
+    print(data)
+    print(prediction)
+    socketio.emit('mqtt_data_update', data)
                   
 
 if __name__ == "__main__":
